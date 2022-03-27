fa785c451809ab70660df6a9e92b85cd6a20f3b8
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-03-27 21:43:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,57 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   return 0;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
   return 0;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = len - 1; j > i; --j) {
+            if (arr[i] + arr[i] == value && (i < j))
+                count++;
+        }
+    }
+    return count;
 }
+
+int poisk(int* arr, int left, int len, int value) {
+  int count = 0, i = left, j = len - 2, sered, x;
+  while (i <= j) {
+    mid = i + (j - i) / 2;
+    if (arr[sered] == value && sered != left && sered != len) {
+      count++;
+      x = sered;
+      while (arr[--sered] == value && sered > left) {
+        count++;
+      }
+      while (arr[++x] == value && x < len) {
+        count++;
+      }
+      return count;
+    } else if (arr[sered] > value) {
+      j = sered - 2;
+    } else {
+      i = sered + 2;
+    }
+  }
+  return 0;;
+}
+
 int countPairs3(int *arr, int len, int value) {
   return 0;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    count += poisk(arr, i, len, value - arr[i]);
+  }
+  return count;
 }
