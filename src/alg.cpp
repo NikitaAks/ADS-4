94f4a65bbfa0efbc18ff980eadce695a5b4d62e5
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-03-27 22:24:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,57 +3,43 @@ int countPairs1(int *arr, int len, int value) {
   return 0;
   int count = 0;
   for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
+    for (int a = i + 1; a < len; a++) {
+      if (arr[i] + arr[a] == value)
         count++;
-      }
-    }
-  }
-  return count;
-}
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    if (arr[i] <= value) {
-      for (int j = i + 1; j < len - 1; j++) {
-        if (arr[i] + arr[j] == value) {
-          count++;
-        }
-      }
     }
   }
   return count;
 }
 
-int cbinsearch(int* arr, int left, int len, int value) {
-  int count = 0, i = left, j = len - 1, sered, a;
-  while (i <= j) {
-    sered = i + (j - i) / 2;
-    if (arr[sered] == value && sered != left && sered != len) {
-      count++;
-      a = sered;
-      while (arr[--sered] == value && sered > left) {
+int countPairs2(int *arr, int len, int value) {
+  return 0;
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int a = len - 1 ; a > i; a--) {
+      if (arr[i] + arr[a] == value)
         count++;
-      }
-      while (arr[++a] == value && a < len) {
-        count++;
-      }
-      return count;
-    } else if (arr[sered] > value) {
-      j = sered - 1;
-    } else {
-      i = sered + 1;
     }
   }
-  return 0;
+  return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
   return 0;
   int count = 0;
   for (int i = 0; i < len; i++) {
-    count += cbinsearch(arr, i, len, value - arr[i]);
+    int lf = i + 1;
+    int rg = len - 1;
+    while (lf < rg) {
+      if (arr[(lf + rg) / 2] + arr[i] < value) {
+        lf = (lf + rg) / 2 + 1;
+      } else {
+        rg = (lf + rg) / 2;
+      }
+    }
+    while (arr[l] + arr[i] == value) {
+      count++;
+      lf++;
+    }
   }
   return count;
 }
