80f5aa7e112fe262b48a70f8b58bd582dd78fc4b
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-03-27 21:50:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,7 +25,7 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int poisk(int* arr, int left, int len, int value) {
-  int count = 0, i = left, j = len - 2, sered, x;
+  int count = 0, i = left, j = len - 1, sered, x;
   while (i <= j) {
     mid = i + (j - i) / 2;
     if (arr[sered] == value && sered != left && sered != len) {
@@ -39,9 +39,9 @@ int poisk(int* arr, int left, int len, int value) {
       }
       return count;
     } else if (arr[sered] > value) {
-      j = sered - 2;
+      j = sered - 1;
     } else {
-      i = sered + 2;
+      i = sered + 1;
     }
   }
   return 0;;
